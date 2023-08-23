6de7c3e7a8e347087e2c1fbe14788f6e7c2e0f39
hellenwamaitha <hellenwama254@gmail.com>
2023-08-23 21:44:17 +0300

    updated lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,21 @@
 def write_file(file_name, file_content):
-    pass
+    
+        with open(file_name, mode='w') as file:
+            file.txl.write(file_content)
+        print("File written successfully.")
+  
 
 def append_file(file_name, append_content):
-    pass
+    
+        with open(file_name, mode='a') as file:
+            file.write(append_content)
+        print("Content appended successfully.")
+   
 
 def read_file(file_name):
-    pass
+    
+        with open(file_name, mode='r') as file:
+            content = file.read()
+            return content
+   
+                 
